dcf47a0fbe2dc85afdc847021a161113d8c3a34f
websiteaap <53229705+websiteaap@users.noreply.github.com>
2019-10-22 13:08:51 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,25 @@
+def ar():
+    a= 12+56
+    print(a)
+
+ar()
+
+from math import log, e, sin, pi
+print('value of pi = ',pi)
+print("value of e = ",e)
+print ("value of log(12) = ",log(12))
+print("value of sin(90) = ",sin(90))
+
+print("value of sin(0) = ",sin(0))
+print("value of sin(30) = ",sin(30))
+
+print("value of sin(45) = ",sin(45))
+print("value of sin(60) = ",sin(60))
+print("value of sin(120) = ",sin(120))
+
+print("value of sin(135) = ",sin(135))
+
+print("value of sin(145) = ",sin(145))
+
+print("value of sin(180) = ",sin(180))
+print ("value of all avobe operation is addition = ",pi+e+log(12)+sin(90))
